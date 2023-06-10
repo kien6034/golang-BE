3d07ca80d5d4ef953b19e84f0bf825949a5fb99f
kien6034 <trinhtrungkientb99@gmail.com>
2023-06-10 19:08:13 +0700

    feat: add get account and to update account into a single db tx

--- db/query/account.sql
+++ db/query/account.sql
@@ -34,3 +34,10 @@ FOR NO KEY UPDATE;
 -- name: DeleteAccount :exec
 DELETE FROM accounts
 WHERE id = $1;
+
+
+-- name: AddAccountBalance :one
+UPDATE accounts
+SET balance = balance + sqlc.arg(amount)
+WHERE id = sqlc.arg(id)
+RETURNING *;
